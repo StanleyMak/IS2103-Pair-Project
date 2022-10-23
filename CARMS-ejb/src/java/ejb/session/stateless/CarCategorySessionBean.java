9fd3997366f7ae7b6d14e7dc5f938487914916d1
morksen123 <morksen123@gmail.com>
2022-10-23 22:03:13 +0800

    EntityManager bug

---
+++ CARMS-ejb/src/java/ejb/session/stateless/CarCategorySessionBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author hanyang
+ */
+@Stateless
+public class CarCategorySessionBean implements CarCategorySessionBeanRemote, CarCategorySessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    @PersistenceContext(unitName = "CARMS-ejbPU")
+    private EntityManager em;
+    
+}
